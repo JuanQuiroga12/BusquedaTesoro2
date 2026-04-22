d98134f063772b03b2238e3ceac86a048205b988
JuanQuiroga12 <est.juand.quiroga@unimilitar.edu.co>
2026-04-22 01:29:58 -0500

    Arreglo y final

--- Assets/Scripts/UI/ControlVolumen.cs
+++ Assets/Scripts/UI/ControlVolumen.cs
@@ -1,29 +1,64 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
 
 public class ControlVolumen : MonoBehaviour
 {
-    public Slider sliderMusica;
-    public Slider sliderEfectos;
+    public static ControlVolumen Instance { get; private set; }
+
     public AudioSource musicaFondo;
-    public float volumenMaxMusica = 0.1f;
+    public float volumenMaxMusica = 0.3f;
+
+    private float valorMusica = 0.5f;
+    private float valorEfectos = 0.5f;
+
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
 
     void Start()
     {
-        DontDestroyOnLoad(gameObject);
-        sliderMusica.value = 0.5f;
-        sliderEfectos.value = 0.5f;
+        CambiarMusica(valorMusica);
+        CambiarEfectos(valorEfectos);
+    }
+
+    /// <summary>
+    /// Llamado por PanelAjustesConector cuando el panel se activa.
+    /// </summary>
+    public void ConectarSliders(Slider sliderMusica, Slider sliderSonido)
+    {
+        if (sliderMusica != null)
+        {
+            sliderMusica.value = valorMusica;
+            sliderMusica.onValueChanged.RemoveAllListeners();
+            sliderMusica.onValueChanged.AddListener(CambiarMusica);
+        }
+
+        if (sliderSonido != null)
+        {
+            sliderSonido.value = valorEfectos;
+            sliderSonido.onValueChanged.RemoveAllListeners();
+            sliderSonido.onValueChanged.AddListener(CambiarEfectos);
+        }
     }
 
     public void CambiarMusica(float valor)
     {
+        valorMusica = valor;
         if (musicaFondo != null)
             musicaFondo.volume = valor * volumenMaxMusica;
     }
 
     public void CambiarEfectos(float valor)
     {
+        valorEfectos = valor;
         AudioListener.volume = valor;
     }
 }
--- Assets/Scripts/UI/MenuEmergente.cs
+++ Assets/Scripts/UI/MenuEmergente.cs
@@ -1,12 +1,23 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MenuEmergente : MonoBehaviour
 {
     public GameObject panelMenu;
 
+    [Header("Sliders de Ajustes")]
+    [SerializeField] private Slider sliderMusica;
+    [SerializeField] private Slider sliderSonido;
+
     public void AbrirMenu()
     {
         panelMenu.SetActive(true);
+
+        // Reconectar sliders al ControlVolumen cada vez que se abre
+        if (ControlVolumen.Instance != null)
+        {
+            ControlVolumen.Instance.ConectarSliders(sliderMusica, sliderSonido);
+        }
     }
 
     public void CerrarMenu()
